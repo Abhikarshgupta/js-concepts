15e501617f359be27b4e49157c047d9e01747746
Abhikarsh Gupta <abg71297@gmail.com>
2023-04-04 10:13:57 +0530

    implemented compose function that returns an array of results

--- composition/compose.js
+++ composition/compose.js
@@ -5,50 +5,32 @@
  * 3. currying - function that (takes an argument)optional and returns a function.
  * https://www.codementor.io/@michelre/use-function-composition-in-javascript-gkmxos5mj == reference point
  */
-
-const addTwoNumbers = (num1) => (num2) => {
-	console.log({ num1, num2 });
-	return num1 + num2;
-};
-
 const addNumbers = (cb) => (arr) =>
 	arr.reduce((prev, curr) => cb(prev, curr), 0);
 
-// arr.reduce((prev, curr) => prev + curr, 0);
+const multiplyNumbers = (cb) => (arr) =>
+	arr.reduce((prev, curr) => cb(prev, curr), 1);
 
-const multiplyTwoNumbers = (num1) => (num2) => num1 * num2;
-const divideTwoNumbers = (num1) => (num2) => {
-	if (num2 === 0) {
-		return 1;
-	}
-	return num1 / num2;
+const divideNumbers = (cb) => (arr) =>
+	arr.reduce((prev, curr) => cb(prev, curr), 1);
+
+const performComplexCalculation = (functionArr) => (arguments) => {
+	const result = [];
+	functionArr.reduceRight((prev, curr) => {
+		const x = curr(prev);
+		result.push(x);
+		return prev;
+	}, arguments);
+	return result;
 };
 
-// const performComplexCalculation = (functionArr) => (arguments) =>
-// 	functionArr.reduceRight((argument, fn) => {
-// 		const x = arguments.reduceRight((prev, curr) => {
-// 			console.log("called", curr, fn);
-// 			const z = fn(curr);
-// 			const y = z(1);
-// 			return prev + y;
-// 		}, 0);
-// 		console.log({ x });
-// 	}, 0);
-
-// console.log(
-// 	performComplexCalculation([
-// 		multiplyTwoNumbers,
-// 		divideTwoNumbers,
-// 		addTwoNumbers,
-// 	])([1, 12])
-// );
-
-const performComplexCalculation1 = (functionArr) => (arguments) =>
-	functionArr.reduce((prev, curr) => {
-		console.log({ prev, curr });
-	}, arguments);
-// const
-
 console.log(
-	performComplexCalculation1([addNumbers((num1, num2) => num1 + num2)])([1, 2])
+	performComplexCalculation([
+		addNumbers((num1, num2) => num1 + num2),
+		multiplyNumbers((num1, num2) => num1 * num2),
+		divideNumbers((num1, num2) => {
+			if (num2 === 0) return 1;
+			return num1 / num2;
+		}),
+	])([1, 2, 4, 5, 6])
 );
