d7a3ef62cabb170fe52102aa03928408391402c7
Abhikarsh Gupta <abg71297@gmail.com>
2023-04-05 09:09:19 +0530

    solve question 1

--- currying/index.js
+++ currying/index.js
@@ -22,3 +22,28 @@ console.log(add(1)(2));
  * 1. how will i call this function n times?
  * 2. how will this function store result of each call? should result be calculated at the end through array of params? or should there be a return at each level?
  */
+// solving problem 1.
+
+const numbers = [1, 2, 3, 4, 5];
+
+// dummy function that will do currying.
+const addAllNumberInputs = (input) => {
+	if (!Number.isNaN(input)) {
+		// add this to current sum.
+	}
+};
+// approach 1:- call function manually as hardcoded.
+addAllNumberInputs(1)(2)(3)(4)(5);
+
+// approach 2. call function through a loop, use callbacks efficiently.
+const addMultipleNumbers = (inputNums) => {
+	let currResult = null;
+	for (let i = 1; i < inputNums.length; i++) {
+		currResult = addAllNumberInputs(inputNums[i]);
+	}
+	// final function to actually get the final answer.
+	return currResult();
+};
+addAllNumberInputs(numbers);
+
+// approach 2 is extensible to solve problem :- how will i call this function n times?.
