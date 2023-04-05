b0629d307c77b0c214dfbd8478a5b203e96b966b
Abhikarsh Gupta <abg71297@gmail.com>
2023-04-05 09:39:28 +0530

    implemented extensible and reusable currying (infinite)

--- currying/index.js
+++ currying/index.js
@@ -13,7 +13,7 @@ const add = (num1) => {
 	};
 };
 
-console.log(add(1)(2));
+console.log("output of basic currying:- ", add(1)(2));
 
 /**
  * the above is naive approach and not extensible. or reusable.
@@ -33,7 +33,10 @@ const addAllNumberInputs = (input) => {
 	}
 };
 // approach 1:- call function manually as hardcoded.
-addAllNumberInputs(1)(2)(3)(4)(5);
+
+{
+	/*addAllNumberInputs(1)(2)(3)(4)(5)();*/
+}
 
 // approach 2. call function through a loop, use callbacks efficiently.
 const addMultipleNumbers = (inputNums) => {
@@ -44,6 +47,61 @@ const addMultipleNumbers = (inputNums) => {
 	// final function to actually get the final answer.
 	return currResult();
 };
-addAllNumberInputs(numbers);
+
+{
+	/*addAllNumberInputs(numbers);*/
+}
 
 // approach 2 is extensible to solve problem :- how will i call this function n times?.
+
+/**
+ * Solving 2. how will this function store result of each call? should result be calculated at the end through array of params? or should there be a return at each level?
+ * since we are not sure of the number of times this function will be called, we need a solution that doesn't care about length of array etc.
+ * this curried function should only depend on 2 inputs at a time. and return it's calculation..
+ * Recursion comes to mind.
+ * what will be the terminating condition? --> when no input is sent, return the current running sum..
+ * otherwise, just call the function with sum of previous instance! works even with 1 input.
+ */
+
+const addAllNumberInputsImplemented = (inputNum1) => {
+	return (inputNum2) => {
+		// terminating condition
+		if (!inputNum2) {
+			// this will contain the running sum.
+			return inputNum1;
+		}
+		if (!isNaN(inputNum1) && !isNaN(inputNum2))
+			return addAllNumberInputsImplemented(inputNum1 + inputNum2);
+		// if wrong inputs, return -1.
+		return -1;
+	};
+};
+
+// calling with hardcoded.
+
+console.log(
+	"output of extensible currying:- ",
+	addAllNumberInputsImplemented(1)(2)(3)(4)(5)()
+);
+
+// calling with function looping.
+
+const addMultipleNumbersImplemented = (numbersList) => {
+	let currentFunctionResult = addAllNumberInputsImplemented(numbersList[0]);
+	for (let i = 1; i < numbersList.length; i++) {
+		currentFunctionResult = currentFunctionResult(numbersList[i]);
+	}
+	if (typeof currentFunctionResult === "number" && currentFunctionResult === -1)
+		return "Wrong inputs received.";
+	return currentFunctionResult();
+};
+
+console.log(
+	"output of extensible and reusable currying:- ",
+	addMultipleNumbersImplemented(numbers)
+);
+
+console.log(
+	"output of extensible and reusable currying (wrong inputs):- ",
+	addMultipleNumbersImplemented([...numbers, "asdf"])
+);
